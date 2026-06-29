8779bffb5405562ef340b336399461960e158c15
robin <comfysage@isabelroses.com>
2026-06-29 14:30:25 +0200

    wip

--- keyboards/lily58/keymaps/comfysage/keymap.c
+++ keyboards/lily58/keymaps/comfysage/keymap.c
@@ -1,16 +1,12 @@
 #include QMK_KEYBOARD_H
 
-#define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
-
 enum layer_number {
-  _BASE = L_BASE,
+  _BASE = 0,
   _LOWER,
   _RAISE,
   _ADJUST,
+  _COILMAK,
+  _MATRIX
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -21,64 +17,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   '  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | LSPC |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | RSPC |
+ * | LSFT |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | RSFT |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * | LCTL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LALT |LOWER | / SPC   /       \Enter \  |RAISE | BSPC |      |
+ *                   | LGUI |LOWER | LALT | / SPC   /       \Enter \  | BSPC |RAISE |COILMA|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_BASE] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
-  SC_LSPO,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SC_RSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-                        KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, _______
+                        KC_LGUI, MO(_LOWER), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, MO(_RAISE), TT(_COILMAK)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * | F11  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |      |   \  |   |  |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   \  |   |  |   `  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|   _  |   -  |   +  |   =  |      |   `  |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------|   _  |   -  |   +  |   =  |      |      |
+ * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LGUI  | LALT |LOWER | / SPC   /       \Enter \  |RAISE | BSPC |      |
+ *                   | LGUI |LOWER | LALT | / SPC   /       \Enter \  | BSPC |RAISE |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_LOWER] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, KC_BSLS, KC_PIPE,
-  _______, _______, _______, _______, _______, _______,                   KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  XXXXXXX, KC_GRV,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______,
-                             _______, _______, _______, _______, _______, _______, _______, _______
+  KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+  _______, _______, _______, _______, _______, _______,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_PIPE, KC_GRV,
+  _______, _______, _______, _______, _______, _______,                   KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  XXXXXXX, _______,
+  _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______,
+                             _______, _______, _______, _______, _______, KC_DEL,  _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      | VOLD | VOLU | MUTE |      |      |                    |      |      | PREV | PLAY | NEXT | PSCR |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------| Left | Down |  Up  |Right |      |      |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
+ * |      |  F5  |  F6  |  F7  |  F8  |      |-------.    ,-------| Left | Down |  Up  |Right |      |      |
+ * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
+ * |      |  F9  |  F10 |  F11 |  F12 |      |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LGUI  | LALT |LOWER | / SPC   /       \Enter \  |RAISE | BSPC |      |
+ *                   | LGUI |LOWER | LALT | / SPC   /       \Enter \  | BSPC |RAISE |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_RAISE] = LAYOUT(
-  _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______,                     _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_PSCR,
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_ESC,  KC_VOLD, KC_VOLU, KC_MUTE, _______, _______,                     _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_PSCR,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                     _______, _______, _______, _______, _______, _______,
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
 /* ADJUST
@@ -91,16 +87,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |   1  |   2  |   3  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LGUI  | LALT |LOWER | / SPC   /       \Enter \  |RAISE | BSPC |      |
+ *                   | LGUI |LOWER | LALT | / SPC   /       \Enter \  | BSPC |RAISE |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_ADJUST] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   XXXXXXX,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P4,   KC_P5,   KC_P6,   _______,
+  _______, _______, _______, _______, _______, _______, KC_LBRC,  KC_RBRC, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______,
                              _______, _______, _______, _______, _______,  _______, _______, _______
+),
+[_COILMAK] = LAYOUT(
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+  _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_BSLS,
+  _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_SCLN,
+  _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+                             _______, MO(_MATRIX), _______, _______, _______,  _______, _______, _______
+),
+[_MATRIX] = LAYOUT(
+  _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                             _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
 
@@ -108,7 +118,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
-//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
+// SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -123,21 +133,26 @@ char layer_state_str[24];
 
 const char *read_layer_state(void) {
   switch (layer_state) {
-    case L_BASE:
+    case _BASE:
         snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Base");
         break;
-    case L_LOWER:
+    case _LOWER:
         snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
         break;
-    case L_RAISE:
+    case _RAISE:
         snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
         break;
-    case L_ADJUST:
-    case L_ADJUST_TRI:
+    case _ADJUST:
         snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
         break;
+    case _COILMAK:
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Coilmak");
+        break;
+    case _MATRIX:
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Matrix");
+        break;
     default:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: MO(%01d)", get_highest_layer(layer_state));
   }
 
   return layer_state_str;
