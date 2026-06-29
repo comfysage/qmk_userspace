81b287cffc63ec84d1f416c3a826a5511d87f2e8
robin <comfysage@isabelroses.com>
2026-06-29 14:29:14 +0200

    config: tapping options

--- keyboards/lily58/keymaps/comfysage/config.h
+++ keyboards/lily58/keymaps/comfysage/config.h
@@ -20,5 +20,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+#define TAPPING_TERM 150
+#define TAPPING_TOGGLE 2
